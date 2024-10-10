667862d4ffa5573b94bc30829b2e528c07d62993
miftahadzdzaudanislam <miftahdzaudan@gmail.com>
2024-10-10 18:49:05 +0800

    tugas pertemuan 3

--- tugas3/animal.php
+++ tugas3/animal.php
@@ -3,12 +3,12 @@ class Animal{
     // Property
     public $namaHewan = [];
 
-    // Methode Constructor
+    // Method Constructor
     public function __construct($data){
         $this->namaHewan = $data;
     }
 
-    // Methode Index untuk menampilkan data
+    // Method Index untuk menampilkan data
     public function index(){
         foreach ($this->namaHewan as $hewan) {
             echo 'Nama Hewan : '. $hewan;
@@ -17,17 +17,17 @@ class Animal{
         return;
     }
 
-    // Methode Store untuk menambahkan data hewan
+    // Method Store untuk menambahkan data hewan
     public function store($data){
         array_push($this->namaHewan, $data);
     }
 
-    // Methode Update untuk mengubah data hewan
+    // Method Update untuk mengubah data hewan
     public function update($index, $data){
         $this->namaHewan[$index] = $data;
     }
 
-    // Methode Destroy untuk menghapus data hewan
+    // Method Destroy untuk menghapus data hewan
     public function destroy($index){
         array_splice($this->namaHewan, $index, 1);
     }
--- tugas3/index.php
+++ tugas3/index.php
@@ -1,23 +1,28 @@
 <?php
 require 'animal.php';
 
+// Membuat objek hewan
 $listHewan = new Animal(['Ayam', 'Kucing', 'Pinguin']);
 
-echo 'Methode Index untuk menampilkan data hewan<br>';
+// menampilkan data hewan
+echo 'Method Index untuk menampilkan data hewan<br>';
 $listHewan->index();
 echo '<br>';
 
-echo 'Methode Store untuk menambahkan data hewan<br>';
+// menampilkan data hewan yang sudah ditambah
+echo 'Method Store untuk menambahkan data hewan<br>';
 $listHewan->store('Burung');
 $listHewan->index();
 echo '<br>';
 
-echo 'Methode Update untuk mengubah data hewan<br>';
+// menampilkan data hewan yang sudah di ubah
+echo 'Method Update untuk mengubah data hewan<br>';
 $listHewan->update(0, 'Jerapah');
 $listHewan->index();
 echo '<br>';
 
-echo 'Methode Destroy untuk menghapus data hewan<br>';
+// menampilkan data hewan yang sudah ada yang hapus
+echo 'Method Destroy untuk menghapus data hewan<br>';
 $listHewan->destroy(1);
 $listHewan->index();
 echo '<br>';
