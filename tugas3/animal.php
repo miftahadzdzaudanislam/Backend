919b3a9416da19d7b5f4e650c624fd28d0574d7a
miftahadzdzaudanislam <miftahdzaudan@gmail.com>
2024-10-10 14:33:39 +0800

    tugas Pertemuan 3

---
+++ tugas3/animal.php
@@ -0,0 +1,34 @@
+<?php
+class Animal{
+    // Property
+    public $namaHewan = [];
+
+    // Methode Constructor
+    public function __construct($data){
+        $this->namaHewan = $data;
+    }
+
+    // Methode Index untuk menampilkan data
+    public function index(){
+        foreach ($this->namaHewan as $hewan) {
+            echo 'Nama Hewan : '. $hewan;
+            echo '<br>';
+        }
+        return;
+    }
+
+    // Methode Store untuk menambahkan data hewan
+    public function store($data){
+        array_push($this->namaHewan, $data);
+    }
+
+    // Methode Update untuk mengubah data hewan
+    public function update($index, $data){
+        $this->namaHewan[$index] = $data;
+    }
+
+    // Methode Destroy untuk menghapus data hewan
+    public function destroy($index){
+        array_splice($this->namaHewan, $index,1);
+    }
+}
---
+++ tugas3/index.php
@@ -0,0 +1,23 @@
+<?php
+require 'animal.php';
+
+$listHewan = new Animal(['Ayam', 'Kucing', 'Anjing']);
+
+echo 'Methode Index untuk menampilkan data hewan<br>';
+$listHewan->index();
+echo '<br>';
+
+echo 'Methode Store untuk menambahkan data hewan<br>';
+$listHewan->store('Burung');
+$listHewan->index();
+echo '<br>';
+
+echo 'Methode Update untuk mengubah data hewan<br>';
+$listHewan->update(0, 'Babi');
+$listHewan->index();
+echo '<br>';
+
+echo 'Methode Destroy untuk menghapus data hewan<br>';
+$listHewan->destroy(1);
+$listHewan->index();
+echo '<br>';
