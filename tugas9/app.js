761a4b4517be596d0cb85606ec02bdc3c9e42f04
miftahadzdzaudanislam <miftahdzaudan@gmail.com>
2024-12-04 13:06:25 +0800

    Pertemuan 9

---
+++ tugas9/app.js
@@ -0,0 +1,17 @@
+var {index, store, update, destroy} = require("./FruitController");
+
+const main = () => {
+    console.log("Metod index - Menampilkan Buah")
+    index();
+    console.log()
+    console.log("Metod store - Menambah buah Pisang")
+    store("Pisang");
+    console.log()
+    console.log("Metod update - Update data 0 menjadi kelapa");
+    update(0, "Kelapa");
+    console.log()
+    console.log("Metod destroy - Menghapus data 0");
+    destroy(0);
+};
+
+main();
---
+++ tugas9/fruits.js
@@ -0,0 +1,3 @@
+var fruits = ["Jeruk", "Apel", "Durian"];
+
+module.exports = fruits;
