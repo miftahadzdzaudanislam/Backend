83f5f2a76c0374ada7b97a1d881afdd8ea5939cc
miftahadzdzaudanislam <miftahdzaudan@gmail.com>
2024-12-11 15:50:50 +0800

    pertemuan 10

--- tugas10/task10.js
+++ tugas10/task10.js
@@ -1,1 +1,21 @@
-const a = 1
+const showDownload = (result) => {
+    console.log("Download Selesai.");
+    console.log(`Hasil download: ${result}`);
+}
+
+// Promise
+const download = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("windows-10.exe");
+        }, 3000);
+    });
+}
+
+// Async Await
+async function main() {
+    const result = await download();
+    showDownload(result);
+}
+
+main();
